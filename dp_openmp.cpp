9b8c33c019a47280c2e93a640370223ab9ab0a74
Superdanby <dannymrt1@gmail.com>
2019-03-06 02:16:58 +0800

    Clean up

--- dp_openmp.cpp
+++ dp_openmp.cpp
@@ -3,38 +3,34 @@
 #include <cstdio>
 #include <cstring>
 #include <iostream>
+#include <thread>
+
 using namespace std;
-const int MAXN = 10;
+
+const int MAXN = 7;
 static char A[MAXN], B[MAXN];
 #define DP_TYPE int
-int lcs_len(const char *A, int na, const char *B, int nb, int dpf[]) {
-    const int match = 8, mismatch = -5, shift = -3;
+const int match = 8, miss = -5, shift = -3;
+const int nthreads = std::thread::hardware_concurrency() > 1 ? std::thread::hardware_concurrency() - 1 : 1;
+
+int bio_match(const char *A, int na, const char *B, int nb) {
     if (na > nb) swap(A, B), swap(na, nb);
-    static DP_TYPE dp[3][MAXN << 1];
-    for (int i = 0; i < 3; i++)
-        memset(dp[i], 0, sizeof(DP_TYPE) * (nb + na + 2));
-    memset(dpf, 0, sizeof(DP_TYPE) * (nb + 1));
-    omp_set_num_threads(8);
+    static DP_TYPE dp[3][MAXN];
+    omp_set_num_threads(nthreads);
     int last_l = 0, last_r = 0;
+    dp[0][0] = 0;
     dp[1][1] = dp[1][0] = shift;
     for (int i = 0; i < na + nb - 1; i++) {
-        // 0 [na - 1] 0 [nb] nb, 0 [nb - 1] nb - 1 [na] nb - 1
         int l = max(0, i - na + 1), r = min(i, nb - 1);
         dp[2][r + 2] = dp[2][0] = (i + 2) * shift;
 #pragma omp parallel for schedule(static) firstprivate(na, A, B)
         for (int j = l; j <= r; j++) {
             int x = i - j, y = j;
-            // printf("x = %d, y = %d\n", x, y);
-            dp[2][j + 1] = dp[0][j] + (A[x] == B[y] ? match : mismatch);
+            dp[2][j + 1] = dp[0][j] + (A[x] == B[y] ? match : miss);
             dp[2][j + 1] = max(
                 dp[2][j + 1],
                 (dp[1][j] > dp[1][j + 1] ? dp[1][j] : dp[1][j + 1]) + shift);
         }
-        if (i - l == na - 1) dpf[l + 1] = dp[2][l + 1];
-        // for (auto w : dp[2]) printf("%4d ", w);
-        // printf("\n");
-        // for (int z = 0; z < MAXN; z++) printf("%4d ", dpf[z]);
-        // printf("\n");
         memcpy(dp[0] + last_l, dp[1] + last_l,
                sizeof(DP_TYPE) * (last_r - last_l + 3));
         memcpy(dp[1] + l, dp[2] + l, sizeof(DP_TYPE) * (r - l + 3));
@@ -42,12 +38,12 @@ int lcs_len(const char *A, int na, const char *B, int nb, int dpf[]) {
     }
     return dp[2][nb];
 }
+
 int main() {
-    int dp[MAXN];
     while (scanf("%s %s", A, B) == 2) {
         string a = A, b = B;
-        int len = lcs_len(a.c_str(), strlen(A), b.c_str(), strlen(B), dp);
-        printf("%d\n", len);
+        int score = bio_match(a.c_str(), strlen(A), b.c_str(), strlen(B));
+        printf("%d\n", score);
     }
     return 0;
 }
